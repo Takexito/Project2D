ecc6e8a3b7b0475cc6ab1731e95632c3ede2ef0f
Yare-Yare-Daze <thebestworkerinthecountry@gmail.com>
2019-11-03 15:30:06 +0300

    Add scripts for box and camera

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+
+    private Rigidbody2D rb2d;
+    private GameObject enemy;
+    private float currentSpeed;
+    [SerializeField] private float speedForDestroy = 2;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+    }
+
+    void Update()
+    {
+        currentSpeed = rb2d.velocity.magnitude;
+    }
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (currentSpeed >= speedForDestroy)
+        {
+            if (other.gameObject == enemy)
+            {
+                Debug.Log("collision");
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (currentSpeed >= speedForDestroy)
+        {
+            if (other.tag == "Wall")
+            {
+                Debug.Log("trigger");
+                Destroy(gameObject);
+            }
+        }
+    }
+}
